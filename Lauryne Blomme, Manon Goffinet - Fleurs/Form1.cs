e7edc7dca6a60b6a0f8e7728db4258faae08908a
PC1 <PC1@LAPTOP-7UFF3350>
2023-04-26 23:19:53 +0200

    Ajoutez des fichiers projet.

---
+++ Lauryne Blomme, Manon Goffinet - Fleurs/Form1.cs
@@ -0,0 +1,10 @@
+namespace Lauryne_Blomme__Manon_Goffinet___Fleurs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lauryne Blomme, Manon Goffinet - Fleurs/Program.cs
@@ -0,0 +1,17 @@
+namespace Lauryne_Blomme__Manon_Goffinet___Fleurs
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
